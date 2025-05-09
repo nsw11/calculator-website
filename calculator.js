f9d8d9637536357768fadf8897236cbc8a812c1e
nsw11 <nswen@ucsc.edu>
2025-05-09 13:23:17 -0700

    Added html and js files

---
+++ calculator.js
@@ -0,0 +1,22 @@
+function add(num,denom)
+{
+    return num + denom;
+}
+
+function sub(num,denom)
+{
+    return num - denom;
+}
+
+function mult(num,denom)
+{
+    return num * denom;
+}
+
+function div(num,denom)
+{
+    if (denom == 0)
+    {
+        console.log("ERROR: Cannot divide by zero")
+    }
+}
